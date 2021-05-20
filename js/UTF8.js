d24fa5f960d15b224aeda8ac6bf1298aeb4eaa2b
GalinKalinovY <gyordanov@almata.cat>
2021-05-20 17:48:03 +0200

    Tenim feta la part del getuser info i tambe la del get session correctament amb els apisigs, tokens i etc.. Ara estem treballant en el html del index.

--- js/UTF8.js
+++ js/UTF8.js
@@ -4,26 +4,30 @@
  *  http://www.webtoolkit.info/
  *
  **/
-function encode(string) {
+var Utf8 = {
     // public method for url encoding
-        string = string.replace(/\r\n/g, "\n");
+    encode : function (string) {
+        string = string.replace(/\r\n/g,"\n");
         var utftext = "";
         for (var n = 0; n < string.length; n++) {
             var c = string.charCodeAt(n);
             if (c < 128) {
                 utftext += String.fromCharCode(c);
-            } else if ((c > 127) && (c < 2048)) {
+            }
+            else if((c > 127) && (c < 2048)) {
                 utftext += String.fromCharCode((c >> 6) | 192);
                 utftext += String.fromCharCode((c & 63) | 128);
-            } else {
+            }
+            else {
                 utftext += String.fromCharCode((c >> 12) | 224);
                 utftext += String.fromCharCode(((c >> 6) & 63) | 128);
                 utftext += String.fromCharCode((c & 63) | 128);
             }
         }
         return utftext;
-}
-function decode(utftext) {
+    },
+    // public method for url decoding
+    decode : function (utftext) {
         var string = "";
         var i = 0;
         var c = c1 = c2 = 0;
@@ -46,4 +50,5 @@ function decode(utftext) {
             }
         }
         return string;
+    }
 }
--- js/main.js
+++ js/main.js
@@ -1,89 +1,100 @@
-var myAPI_key="eadafa1e4ae708f5f7046192a4602074";
-var myshared_secret="b19b6efed21c4110264fa2393e26bd3a";
-
+const myAPI_key="eadafa1e4ae708f5f7046192a4602074";
+const myshared_secret="b19b6efed21c4110264fa2393e26bd3a";
 var url = window.location.href; // or window.location.href for current url
 var captured = /token=([^&]+)/.exec(url)[1]; // Value is in [1] ('384' in our case)
 var result = captured ? captured : 'myDefaultValue';
-console.log(captured);
+const urlAudio = "http://ws.audioscrobbler.com/2.0/?";
 
-function func()
-{
-    // Set elsewhere but hacked into this example:
-    var last_fm_data = {
-        'last_token':captured,
-        'user': 'Gkalinov',
-        'secret': myshared_secret
+function loadData() {
+
+    //Crec que només necessitem apikey, token i secret i no necessitem params, els podem treure de sessionStorage
+    //Calcula l'apiSig a partir dels valors d'abans...
+
+    var data = {
+        'token':  Utf8.encode(captured),
+        'api_key': myAPI_key,
+        'method': 'auth.getSession'
     };
-    last_fm_call('auth.getSession', {'token': last_fm_data['last_token']});
 
-    // Low level API call, purely builds a POSTable object and calls it.
-    function last_fm_call(method, data){
+    data["api_sig"] = calculateApiSig(data);
+    data["format"] = "json";
 
-        // Append some static variables
-        data.api_key = myAPI_key;
-        //data['format'] = 'json';
-        data['method'] = method;
 
-        post_data = last_fm_calculate_apisig(data);
-        /*
-        .*/
-        console.log("Post data: Last token " + post_data.token + "ApiKey: "+ post_data.api_key + "ApiSig: " + post_data.api_sig);
-        sessionStorage.setItem("myApiSig",post_data.api_sig );
+    var last_url = urlAudio;
 
-        var last_url="http://ws.audioscrobbler.com/2.0/?";
-        $.ajax({
-            type: "GET",
-            url: last_url,
-            data : 'method=auth.getSession' +
-                '&token='+
-                captured+
-                '&api_key='+ myAPI_key +
-                '&api_sig='+
-                post_data.api_sig+
-                '&format=json',
-            //data: post_data,
-            dataType: 'json',
-            //"success" gets called when the returned code is a "200" (successfull request). "error" gets called whenever another code is returned (e.g. 404, 500).
-            success: function(res){
-                //No caldria aquesta instrucció perque ja guaredem els que ens convé en sessionStorage
-                last_fm_data[method] = res;
-                //var	myresposta = JSON.parse(res);
-                console.log("Resposta: Name " + res.session.name);// Should return session key.
-                console.log("Resposta: Key " + res.session.key);
+    $.ajax({
+        type: "GET",
+        url: last_url,
+        data: data,
+        dataType: 'json',
+        //"success" gets called when the returned code is a "200" (successfull request). "error" gets called whenever another code is returned (e.g. 404, 500).
+        success: function (res) {
 
-                //store session key for further authenticate operations...
-                sessionStorage.setItem("mySessionUser", res.session.name);
-                sessionStorage.setItem("mySessionKey", res.session.key);
-            },
-            error : function(xhr, status, error){
-                var errorMessage = xhr.status + ': ' + xhr.statusText
-                console.log('Error - ' + errorMessage);
-            }
-        });
-    }
+            console.log("Resposta: Name " + res.session.name);// Should return session key.
+            console.log("Resposta: Key " + res.session.key);
 
-    function last_fm_calculate_apisig(params){
+        },
+        error: function (xhr, status, error) {
+            var errorMessage = xhr.status + ': ' + xhr.statusText
+            console.log('Error - ' + errorMessage);
+        }
+    });
+    //reset data o data2
+    var data2 = {
+        'user': 'Gkalinov',
+        'api_key': myAPI_key
+    };
+    data2["method"] = "user.getInfo";
+    data2["format"] = "json";
 
-        //Crec que només necessitem apikey, token i secret i no necessitem params, els podem treure de sessionStorage
-        //Calcula l'apiSig a partir dels valors d'abans...
-        ss = "";
-        st = [];
-        so = {};
-        so['api_key'] = params['api_key'];
-        so['token'] = params['token'];
-        Object.keys(params).forEach(function(key){
-            st.push(key); // Get list of object keys
-        });
-        st.sort(); // Alphabetise it
-        st.forEach(function(std){
-            ss = ss + std + params[std]; // build string
-        });
-        ss += last_fm_data['secret'];
-        //fem el utf8 aqui.
+    $.ajax({
+        user: "GET",
+        url: last_url,
+        data: data2,
+        dataType: 'json',
+        //"success" gets called when the returned code is a "200" (successfull request). "error" gets called whenever another code is returned (e.g. 404, 500).
+        success: function (res) {
 
-        var hashed_sec = md5(unescape(encodeURIComponent(ss))); // "2063c1608d6e0baf80249c42e2be5804"
-        console.log("La apiSig es: " + hashed_sec);
-        so['api_sig'] = hashed_sec; // Correct when calculated elsewhere.
-        return so; // Returns signed POSTable object
-    }
+            console.log("Resposta: Name " + res.user.name);// Should return session key.
+            console.log("Resposta: image " + res.user.url);
+            console.log("Resposta: image " + res.user.image[2]['#text']);
+
+        },
+        error: function (xhr, status, error) {
+            var errorMessage = xhr.status + ': ' + xhr.statusText
+            console.log('Error - ' + errorMessage);
+        }
+    });
+
+}
+
+function calculateApiSig( params) {
+
+    //Crec que només necessitem apikey, token i secret i no necessitem params, els podem treure de sessionStorage
+    //Calcula l'apiSig a partir dels valors d'abans...
+    var stringActual = "";
+    var arrayKeysParams = [];
+
+
+    Object.keys(params).forEach(function (key) {
+        arrayKeysParams.push(key); // Get list of object keys
+    });
+    arrayKeysParams.sort(); // Alphabetise it
+
+    arrayKeysParams.forEach(function (key) {
+        stringActual = stringActual + key + params[key]; // build string
+    });
+
+    console.log("Mi primer chorizo:", stringActual);
+
+    stringActual = stringActual + myshared_secret;
+    console.log("Mi primer chorizo con shared:", stringActual);
+
+    console.log("Mi primer chorizo con shared limpio :", stringActual);
+
+
+    var hashed_sec = md5(unescape(encodeURIComponent(stringActual)));
+    console.log("La apiSig es: " + hashed_sec);
+
+    return hashed_sec; // Returns signed POSTable objec */
 }
