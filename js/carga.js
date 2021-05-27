ef8fc699b06580b59fe753af265b3e1bd3d58fbf
Galin Kalinov <71660913+GalinKalinovY@users.noreply.github.com>
2021-05-27 19:14:17 +0200

    Update carga.js
    JSDoc comentat!

--- js/carga.js
+++ js/carga.js
@@ -1,3 +1,8 @@
+/**
+*  Funcio: - el que fa la funcio es quant es regarregue la pagina es torne a executar el loadData().
+*
+* @param document -  en ajax es el que mira si el document canvia de estat i aixi es torne a executar.
+**/
 $(document).ready(
     function (){
         loadData();
